fc3beb441b1ed2bfd99511c9a54ce7e97c084f71
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-09-22 17:38:00 +0100

    Now with some parameters that take reasonable time

--- EPoptimization/main.py
+++ EPoptimization/main.py
@@ -9,8 +9,8 @@ import pandas as pd
 from mpi4py import MPI
 import booz_xform as bx
 from pathlib import Path
-from scipy.optimize import minimize
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize, basinhopping
 from simsopt import make_optimizable
 from simsopt.mhd import Vmec, Boozer
 from simsopt.util import MpiPartition
@@ -18,12 +18,13 @@ from simsopt.solve import least_squares_mpi_solve, least_squares_serial_solve
 from simsopt.mhd import QuasisymmetryRatioResidual
 from simsopt.objectives import LeastSquaresProblem
 from simsopt.mhd.vmec_diagnostics import vmec_fieldlines
+from neat.fields import Simple
+from neat.tracing import ChargedParticleEnsemble, ParticleEnsembleOrbit_Simple
+mpi = MpiPartition()
+this_path = Path(__file__).parent.resolve()
 def pprint(*args, **kwargs):
     if MPI.COMM_WORLD.rank == 0:
         print(*args, **kwargs)
-mpi = MpiPartition()
-from neat.fields import Simple
-from neat.tracing import ChargedParticleEnsemble, ParticleEnsembleOrbit_Simple
 ############################################################################
 #### Input Parameters
 ############################################################################
@@ -36,18 +37,23 @@ opt_well = False
 opt_iota = False
 
 s_initial = 0.3  # initial normalized toroidal magnetic flux (radial VMEC coordinate)
-nparticles = 128  # number of particles
-tfinal = 1e-5  # total time of tracing in seconds
-nsamples = 10000 # number of time steps
-multharm = 3 # angular grid factor
-ns_s = 3 # spline order over s
+nparticles = 800  # number of particles
+tfinal = 1e-4  # total time of tracing in seconds
+nsamples = 1000 # number of time steps
+multharm = 5 # angular grid factor
+ns_s = 5 # spline order over s
 ns_tp = 3 # spline order over theta and phi
-nper = 1000 # number of periods for initial field line
+nper = 500 # number of periods for initial field line
 npoiper = 100 # number of points per period on this field line
-npoiper2 = 128 # points per period for integrator step
+npoiper2 = 100 # points per period for integrator step
 notrace_passing = 0 # if 1 skips tracing of passing particles, else traces them
 
+nruns_opt_average = 3 # number of particle tracing runs to average over in cost function
+
+regression_test = False
 nruns_robustness = 20 # number of runs when testing for robustness of cost function
+output_path_parameters=os.path.join(this_path, 'opt_parameters.csv') # where to save robustness results
+mult_factor = 3 # factor to multiply each variable in the regression test
 
 iota_target = -0.42
 weight_optEP = 10.0
@@ -71,7 +77,6 @@ vmec.keep_all_files = True
 surf = vmec.boundary
 g_particle = ChargedParticleEnsemble(r_initial=s_initial)
 ######################################
-this_path = Path(__file__).parent.resolve()
 OUT_DIR=os.path.join(this_path,f'out_s{s_initial}_NFP{vmec.indata.nfp}')
 if opt_quasisymmetry: OUT_DIR+=f'_{QA_or_QH}'
 if opt_well: OUT_DIR+=f'_well'
@@ -82,43 +87,80 @@ def EPcostFunction(v: Vmec):
     v.run()
     g_field_temp = Simple(wout_filename=v.output_file, B_scale=B_scale, Aminor_scale=Aminor_scale, multharm=multharm,ns_s=ns_s,ns_tp=ns_tp)
     g_orbits_temp = ParticleEnsembleOrbit_Simple(g_particle,g_field_temp,tfinal=tfinal,nparticles=nparticles,nsamples=nsamples,notrace_passing=notrace_passing,nper=nper,npoiper=npoiper,npoiper2=npoiper2)
-    final_loss_fraction = g_orbits_temp.total_particles_lost
-    print(f'Loss fraction = {final_loss_fraction}')
+    final_loss_fraction_array = []
+    for i in range(nruns_opt_average): final_loss_fraction_array.append(g_orbits_temp.total_particles_lost)
+    final_loss_fraction = np.mean(final_loss_fraction_array)
+    if not regression_test: print(f'Loss fraction = {final_loss_fraction}')
     # print(f'VMEC dofs = {v.x}')
     return final_loss_fraction
 optEP = make_optimizable(EPcostFunction, vmec)
 ######################################
-pprint("Testing robustness of cost function")
-start_time = time.time()
-costfunction_array=[]
-for i in range(nruns_robustness):
-    costfunction_array.append(optEP.J())
-total_time = time.time() - start_time
-params_dict = {
-    'QA_or_QH': QA_or_QH,
-    's_initial': s_initial,
-    'nparticles': nparticles,
-    'tfinal': tfinal,
-    'multharm': multharm,
-    'ns_s': ns_s,
-    'ns_tp': ns_tp,
-    'nper': nper,
-    'npoiper': npoiper,
-    'npoiper2': npoiper2,
-    'notrace_passing': notrace_passing,
-    'nruns': nruns_robustness,
-    'total_time': total_time,
-    'std_cost_function': np.std(costfunction_array),
-    'mean_cost_function': np.mean(costfunction_array),
-    'coefficient_variation': np.std(costfunction_array)/np.mean(costfunction_array)
-}
-output_path_parameters=os.path.join(this_path, 'opt_parameters.csv')
-print(output_path_parameters)
-df = pd.DataFrame(data=[params_dict])
-if not os.path.exists(output_path_parameters): pd.DataFrame(columns=df.columns).to_csv(output_path_parameters, index=False)
-df.to_csv(output_path_parameters, mode='a', header=False, index=False)
-print(np.std(costfunction_array)/np.mean(costfunction_array))
-# exit()
+if regression_test:
+    initial = 0
+    variables_for_testing = ['initial','nparticles','tfinal','nper','npoiper','npoiper2','nsamples']#,'ns_tp','multharm','ns_s','notrace_passing']
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    n_tests = len(variables_for_testing)
+    perrank = n_tests//size
+    remainder = n_tests % size
+    start_iterator_MPI = rank * perrank + min(rank, remainder)
+    stop_iterator_MPI = (rank + 1) * perrank + min(rank + 1, remainder)
+    comm.Barrier()
+    start_time_tests = time.time()
+    # print(f'Core {rank+1}/{size} is doing',*range(start_iterator_MPI, stop_iterator_MPI),'iterators')
+    for i in range(start_iterator_MPI, stop_iterator_MPI):
+        variable = variables_for_testing[i]
+        # Increase parameters
+        if variable in ['ns_s','ns_tp','multharm']:
+            if eval(f"{variable}")==3: exec(f'{variable}=5')
+            elif eval(f"{variable}")==5: exec(f'{variable}=3')
+            # elif eval(f"{variable}")==4: exec(f'{variable}=5')
+            else: continue
+        elif variable == 'notrace_passing':
+            if notrace_passing == 0: notrace_passing = 1
+            elif notrace_passing == 1: notrace_passing = 0
+        else: exec(f'{variable}=eval("{variable}")*{mult_factor}')
+        # Go
+        print(f'Running set of tests {i+1}/{n_tests} in core {rank+1}/{size} by increasing {variable}')
+        start_time = time.time()
+        costfunction_array=[]
+        for i in range(nruns_robustness):
+            costfunction_array.append(optEP.J())
+        # Write results
+        total_time = time.time() - start_time
+        params_dict = {
+            'QA_or_QH': QA_or_QH,
+            's_initial': s_initial,
+            'nparticles': nparticles,
+            'tfinal': tfinal,
+            'nsamples': nsamples,
+            'multharm': multharm,
+            'ns_s': ns_s,
+            'ns_tp': ns_tp,
+            'nper': nper,
+            'npoiper': npoiper,
+            'npoiper2': npoiper2,
+            'notrace_passing': notrace_passing,
+            'nruns': nruns_robustness,
+            'duration_per_run': total_time/nruns_robustness,
+            'std_J': np.std(costfunction_array),
+            'mean_J': np.mean(costfunction_array),
+            'coeff_variation': np.std(costfunction_array)/np.mean(costfunction_array)
+        }
+        df = pd.DataFrame(data=[params_dict])
+        if not os.path.exists(output_path_parameters): pd.DataFrame(columns=df.columns).to_csv(output_path_parameters, index=False)
+        df.to_csv(output_path_parameters, mode='a', header=False, index=False)
+        # Revert back variables
+        if variable in ['ns_s','ns_tp','multharm']:
+            if eval(f"{variable}")==5: exec(f'{variable}=3')
+            elif eval(f"{variable}")==3: exec(f'{variable}=5')
+            # elif eval(f"{variable}")==4: exec(f'{variable}=3')
+        elif variable == 'notrace_passing':
+            if notrace_passing == 0: notrace_passing = 1
+            elif notrace_passing == 1: notrace_passing = 0
+        else: exec(f'{variable}=eval("{variable}")/{mult_factor}')
+    exit()
 ######################################
 pprint("Initial aspect ratio:", vmec.aspect())
 pprint("Initial mean iota:", vmec.mean_iota())
@@ -139,7 +181,8 @@ pprint("Quasisymmetry objective before optimization:", qs.total())
 ######################################
 initial_dofs=np.copy(surf.x)
 def fun(dofss):
-    prob.x = initial_dofs
+    # prob.x = initial_dofs
+    prob.x = dofss
     return prob.objective()
 for max_mode in max_modes:
     surf.fix_all()
@@ -153,8 +196,9 @@ for max_mode in max_modes:
     pprint('-------------------------')
     pprint(f'Optimizing with max_mode = {max_mode}')
     pprint('-------------------------')
-    res = minimize(fun, dofs, method='BFGS', options={'maxiter': MAXITER}, tol=1e-9)
-    # least_squares_mpi_solve(prob, mpi, grad=True, rel_step=diff_rel_step, abs_step=diff_abs_step, max_nfev=MAXITER)
+    # res = minimize(fun, dofs, method='BFGS', options={'maxiter': MAXITER}, tol=1e-9)
+    # res = basinhopping(fun, dofs, niter=MAXITER, stepsize=0.5, T=1.0)
+    least_squares_mpi_solve(prob, mpi, grad=True, rel_step=diff_rel_step, abs_step=diff_abs_step, max_nfev=MAXITER)
     # least_squares_serial_solve(prob, rel_step=diff_rel_step, abs_step=diff_abs_step, max_nfev=MAXITER)
     ######################################
     pprint("Final aspect ratio:", vmec.aspect())
