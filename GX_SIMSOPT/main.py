6d0a0e121b8e1e0effb8e9f8f9a469e7f9fa7981
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-10-31 12:14:43 +0000

    run

--- GX_SIMSOPT/main.py
+++ GX_SIMSOPT/main.py
@@ -129,7 +129,7 @@ def CalculateHeatFlux(v: Vmec, first_restart=False):
     # gx_cmd = ["srun", "-t", "1:00:00", #"--reservation=gpu2022",
     #             "--gpus-per-task=1", "--ntasks=1", "gx", f"{fnamein}"]
     f_log = os.path.join(OUT_DIR,fname+".log")
-    exit()
+    # exit()
     with open(f_log, 'w') as fp:
         p = subprocess.Popen(gx_cmd,stdout=fp)
 
