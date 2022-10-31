703599c78c1610f668bc6e8e35f22d941585bbdd
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-10-31 14:52:55 +0000

    sa

--- GX_SIMSOPT/main.py
+++ GX_SIMSOPT/main.py
@@ -192,8 +192,6 @@ for max_mode in max_modes:
     pprint('## Now calculating total objective function ##')
     pprint("Total objective before optimization:", prob.objective())
     print('reached here')
-    1/0
-    MPI.Finalize()
     exit()
     pprint('-------------------------')
     pprint(f'Optimizing with max_mode = {max_mode}')
