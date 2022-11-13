7e56bb672d911488fda9862a39e834c441643ea6
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-11-13 20:19:29 +0000

    now creating plots and growth rates

--- GX_SIMSOPT/test_parameters.py
+++ GX_SIMSOPT/test_parameters.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 from shutil import move, copymode
 from joblib import Parallel, delayed
 from simsopt.mhd import Vmec
-from simsopt.turbulence.GX_io import GX_Runner, GX_Output
+# from simsopt.turbulence.GX_io import GX_Runner, GX_Output
 this_path = Path(__file__).parent.resolve()
 ######## INPUT PARAMETERS ########
 gx_executable = '/m100/home/userexternal/rjorge00/gx/gx'
@@ -44,85 +44,75 @@ os.chdir(OUT_DIR)
 vmec = Vmec(vmec_file)
 ## Auxiliary functions
 # Get growth rates
-def getgamma(stellFile, fractionToConsider=0.35):
-    f = netCDF4.Dataset(stellFile,'r',mmap=False)
-    phi2 = np.log(f.variables['phi2'][()])
-    t = f.variables['t'][()]
-    startIndex = int(len(t)*(1-fractionToConsider))
-    mask = np.isfinite(phi2)
-    data_x = t[mask]
-    data_y = phi2[mask]
-    fit = np.polyfit(data_x[startIndex:], data_y[startIndex:], 1)
-    poly = np.poly1d(fit)
-    GrowthRate = fit[0]/2
-    omega_average_array = np.array(f.variables['omega_average'][()])
-    omega_average_array_omega = omega_average_array[-1,:,0,0]
-    omega_average_array_gamma = omega_average_array[-1,:,0,1]
-    max_index = np.argmax(omega_average_array_gamma)
-    gamma = omega_average_array_gamma[max_index]
-    omega = omega_average_array_omega[max_index]
-    # gamma  = np.mean(f.variables['omega'][()][startIndex:,0,0,1])
-    # omega  = np.mean(f.variables['omega'][()][startIndex:,0,0,0])
-    #fitRes = np.poly1d(coeffs)
-    # if not os.path.exists(stellFile+'_phi2.pdf'):
-    plt.figure(figsize=(7.5,4.0))
-    ##############
-    plt.plot(t, phi2,'.', label=r'data - $\gamma_{GS2} = $'+str(gamma))
-    plt.plot(t, poly(t),'-', label=r'fit - $\gamma = $'+str(GrowthRate))
-    ##############
-    plt.legend(loc=0,fontsize=14)
-    plt.xlabel(r'$t$');plt.ylabel(r'$\ln |\hat \phi|^2$')
-    plt.subplots_adjust(left=0.16, bottom=0.19, right=0.98, top=0.97)
-    plt.savefig(stellFile+'_phi2.pdf', format='pdf')
-    plt.close()
-    return GrowthRate, abs(omega)
-# Save final eigenfunction
-def eigenPlot(stellFile):
-    f = netCDF4.Dataset(stellFile,'r',mmap=False)
-    y = f.variables['phi'][()]
-    x = f.variables['theta'][()]
-    plt.figure(figsize=(7.5,4.0))
-    phiR0= y[0,0,int((len(x)-1)/2+1),0]
-    phiI0= y[0,0,int((len(x)-1)/2+1),1]
-    phi02= phiR0**2+phiI0**2
-    phiR = (y[0,0,:,0]*phiR0+y[0,0,:,1]*phiI0)/phi02
-    phiI = (y[0,0,:,1]*phiR0-y[0,0,:,0]*phiI0)/phi02
-    ##############
-    plt.plot(x, phiR, label=r'Re($\hat \phi/\hat \phi_0$)')
-    plt.plot(x, phiI, label=r'Im($\hat \phi/\hat \phi_0$)')
-    ##############
-    plt.xlabel(r'$\theta$');plt.ylabel(r'$\hat \phi$')
-    plt.legend(loc="upper right")
-    plt.subplots_adjust(left=0.16, bottom=0.19, right=0.98, top=0.93)
-    plt.savefig(stellFile+'_eigenphi.pdf', format='pdf')
-    plt.close()
-    return 0
+# def getgamma(stellFile, fractionToConsider=0.35):
+#     f = netCDF4.Dataset(stellFile,'r',mmap=False)
+#     phi2 = np.log(f.variables['phi2'][()])
+#     t = f.variables['time'][()]
+#     startIndex = int(len(t)*(1-fractionToConsider))
+#     mask = np.isfinite(phi2)
+#     data_x = t[mask]
+#     data_y = phi2[mask]
+#     fit = np.polyfit(data_x[startIndex:], data_y[startIndex:], 1)
+#     poly = np.poly1d(fit)
+#     GrowthRate = fit[0]/2
+#     omega_average_array = np.array(f.variables['omega_v_time'][()])
+#     omega_average_array_omega = omega_average_array[-1,:,0,0] # only looking at one kx
+#     omega_average_array_gamma = omega_average_array[-1,:,0,1] # only looking at one kx
+#     max_index = np.argmax(omega_average_array_gamma)
+#     gamma = omega_average_array_gamma[max_index]
+#     omega = omega_average_array_omega[max_index]
+#     # gamma  = np.mean(f.variables['omega'][()][startIndex:,0,0,1])
+#     # omega  = np.mean(f.variables['omega'][()][startIndex:,0,0,0])
+#     #fitRes = np.poly1d(coeffs)
+#     # if not os.path.exists(stellFile+'_phi2.pdf'):
+#     plt.figure(figsize=(7.5,4.0))
+#     ##############
+#     plt.plot(t, phi2,'.', label=r'data - $\gamma_{GS2} = $'+str(gamma))
+#     plt.plot(t, poly(t),'-', label=r'fit - $\gamma = $'+str(GrowthRate))
+#     ##############
+#     plt.legend(loc=0,fontsize=14)
+#     plt.xlabel(r'$t$');plt.ylabel(r'$\ln |\hat \phi|^2$')
+#     plt.subplots_adjust(left=0.16, bottom=0.19, right=0.98, top=0.97)
+#     plt.savefig(stellFile+'_phi2.pdf', format='pdf')
+#     plt.close()
+#     return GrowthRate, abs(omega)
+# # Save final eigenfunction
+# def eigenPlot(stellFile):
+#     f = netCDF4.Dataset(stellFile,'r',mmap=False)
+#     y = f.variables['phi'][()]
+#     x = f.variables['theta'][()]
+#     plt.figure(figsize=(7.5,4.0))
+#     phiR0= y[0,0,int((len(x)-1)/2+1),0]
+#     phiI0= y[0,0,int((len(x)-1)/2+1),1]
+#     phi02= phiR0**2+phiI0**2
+#     phiR = (y[0,0,:,0]*phiR0+y[0,0,:,1]*phiI0)/phi02
+#     phiI = (y[0,0,:,1]*phiR0-y[0,0,:,0]*phiI0)/phi02
+#     ##############
+#     plt.plot(x, phiR, label=r'Re($\hat \phi/\hat \phi_0$)')
+#     plt.plot(x, phiI, label=r'Im($\hat \phi/\hat \phi_0$)')
+#     ##############
+#     plt.xlabel(r'$\theta$');plt.ylabel(r'$\hat \phi$')
+#     plt.legend(loc="upper right")
+#     plt.subplots_adjust(left=0.16, bottom=0.19, right=0.98, top=0.93)
+#     plt.savefig(stellFile+'_eigenphi.pdf', format='pdf')
+#     plt.close()
+#     return 0
 ##### Function to obtain gamma and omega for each ky
-def gammabyky(stellFile,fractionToConsider=0.6):
+def gammabyky(stellFile):
     # Compute growth rate:
     fX   = netCDF4.Dataset(stellFile,'r',mmap=False)
-    tX   = fX.variables['t'][()]
+    tX   = fX.variables['time'][()]
     kyX  = fX.variables['ky'][()]
-    phi2_by_kyX  = fX.variables['phi2_by_ky'][()]
-    omegaX  = fX.variables['omega'][()]
-    startIndexX  = int(len(tX)*(1-fractionToConsider))
-    growthRateX  = []
-    ## assume that kyX=kyNA
-    for i in range(len(kyX)):
-        maskX  = np.isfinite(phi2_by_kyX[:,i])
-        data_xX = tX[maskX]
-        data_yX = phi2_by_kyX[maskX,i]
-        fitX  = np.polyfit(data_xX[startIndexX:], np.log(data_yX[startIndexX:]), 1)
-        thisGrowthRateX  = fitX[0]/2
-        growthRateX.append(thisGrowthRateX)
-    # Compute real frequency:
-    realFreqVsTimeX  = []
-    realFrequencyX   = []
-    for i in range(len(kyX)):
-        realFreqVsTimeX.append(omegaX[:,i,0,0])
-        realFrequencyX.append(np.mean(realFreqVsTimeX[i][startIndexX:]))
+    omega_average_array = np.array(fX.variables['omega_v_time'][()])
+    realFrequencyX = omega_average_array[-1,:,0,0] # only looking at one kx
+    growthRateX = omega_average_array[-1,:,0,1] # only looking at one kx
+    max_index = np.argmax(growthRateX)
+    max_growthrate_gamma = growthRateX[max_index]
+    max_growthrate_omega = realFrequencyX[max_index]
+    max_growthrate_ky = kyX[max_index]
+
     numRows = 1
-    numCols = 2
+    numCols = 3
 
     plt.subplot(numRows, numCols, 1)
     plt.plot(kyX,growthRateX,'.-')
@@ -132,12 +122,20 @@ def gammabyky(stellFile,fractionToConsider=0.6):
     plt.rc('font', size=8)
     plt.rc('axes', labelsize=8)
     plt.rc('xtick', labelsize=8)
-    # plt.legend(frameon=False,prop=dict(size='xx-small'),loc=0)
 
     plt.subplot(numRows, numCols, 2)
     plt.plot(kyX,realFrequencyX,'.-')
     plt.xlabel(r'$k_y$')
     plt.ylabel(r'$\omega$')
+    plt.xscale('log')
+    plt.rc('font', size=8)
+    plt.rc('axes', labelsize=8)
+    plt.rc('xtick', labelsize=8)
+
+    plt.subplot(numRows, numCols, 3)
+    for count, ky in enumerate(kyX): plt.plot(tX,omega_average_array[:,count,0,0],'.-', label=r'$\gamma$ at ky=$'+str(ky))
+    plt.xlabel(r'$t$')
+    plt.ylabel(r'$\gamma$')
     plt.xscale('log')
     plt.rc('font', size=8)
     plt.rc('axes', labelsize=8)
@@ -148,7 +146,7 @@ def gammabyky(stellFile,fractionToConsider=0.6):
     #plt.subplots_adjust(left=0.14, bottom=0.15, right=0.98, top=0.96)
     plt.savefig(stellFile+"_GammaOmegaKy.pdf", format='pdf')
     plt.close()
-    return kyX, growthRateX, realFrequencyX
+    return max_growthrate_gamma, max_growthrate_omega, max_growthrate_ky
 # Function to replace text in a file
 def replace(file_path, pattern, subst):
     fh, abs_path = mkstemp()
@@ -170,10 +168,11 @@ def create_gx_inputs(nzgrid, npol, nstep, dt, nhermite, nlaguerre):
     replace(os.path.join(OUT_DIR,'gx-geometry-sample.ing'),'desired_normalized_toroidal_flux = 0.12755',f'desired_normalized_toroidal_flux = {desired_normalized_toroidal_flux:.3f}')
     replace(os.path.join(OUT_DIR,'gx-geometry-sample.ing'),'vmec_file = "wout_gx.nc"',f'vmec_file = "{f_wout}"')
     replace(os.path.join(OUT_DIR,'gx-geometry-sample.ing'),'alpha = 0.0"',f'alpha = {alpha_fieldline}')
-    shutil.copy(os.path.join(this_path,'convert_VMEC_to_GX'),os.path.join(OUT_DIR,'convert_VMEC_to_GX'))
+    shutil.copy(convert_VMEC_to_GX,os.path.join(OUT_DIR,'convert_VMEC_to_GX'))
     p = subprocess.Popen(f"./convert_VMEC_to_GX gx-geometry-sample".split(),stderr=subprocess.STDOUT,stdout=subprocess.DEVNULL)
     p.wait()
     gridout_file = f'grid.gx_wout_{f_wout[5:-3]}_psiN_{desired_normalized_toroidal_flux}_nt_{2*nzgrid}'
+    os.remove(os.path.join(OUT_DIR,'convert_VMEC_to_GX'))
     #gx.make_fluxtube(f_wout)
     #tag = f_wout[5:-3]
     #ntheta = gx.inputs['Dimensions']['ntheta']
@@ -215,9 +214,9 @@ def create_gx_inputs(nzgrid, npol, nstep, dt, nhermite, nlaguerre):
     # ## REMOVE ALSO OUTPUT FILE
     # for f in glob.glob('*.out.nc'): remove(f)
 # Function to output inputs and growth rates to a CSV file
-def output_to_csv(nzgrid, npol, nstep, dt, nhermite, nlaguerre, growth_rate, ln, lt):
-    keys=np.concatenate([['ln'],['lt'],['nzgrid'],['npol'],['nlambda'],['nstep'],['nhermite'],['nlaguerre'],['dt'],['growth_rate']])
-    values=np.concatenate([[ln],[lt],[nzgrid],[npol],[nlambda],[nstep],[nhermite],[nlaguerre],[dt],[growth_rate]])
+def output_to_csv(nzgrid, npol, nstep, dt, nhermite, nlaguerre, growth_rate, frequency, ky, ln, lt):
+    keys=np.concatenate([['ln'],['lt'],['nzgrid'],['npol'],['nlambda'],['nstep'],['nhermite'],['nlaguerre'],['dt'],['growth_rate'],['frequency'],['ky']])
+    values=np.concatenate([[ln],[lt],[nzgrid],[npol],[nlambda],[nstep],[nhermite],[nlaguerre],[dt],[growth_rate],[frequency],[ky]])
     dictionary = dict(zip(keys, values))
     df = pd.DataFrame(data=[dictionary])
     if not os.path.exists(output_csv): pd.DataFrame(columns=df.columns).to_csv(output_csv, index=False)
@@ -233,15 +232,16 @@ def run_gx(nzgrid, npol, nstep, dt, nhermite, nlaguerre):
     #p = subprocess.Popen(f"{gx_executable} {os.path.join(OUT_DIR,gx_input_name+'.in')}".split(),stderr=subprocess.STDOUT,stdout=subprocess.DEVNULL)
     p.wait()
     fout = os.path.join(OUT_DIR,gx_input_name+".nc")
-    gx_out = GX_Output(fout)
-    qavg, dqavg = gx_out.exponential_window_estimator()
-    # eigenPlot(os.path.join(OUT_DIR,f"{gs2_input_name}.out.nc"))
-    # growth_rate, omega = getgamma(os.path.join(OUT_DIR,f"{gs2_input_name}.out.nc"))
-    # kyX, growthRateX, realFrequencyX = gammabyky(os.path.join(OUT_DIR,f"{gs2_input_name}.out.nc"))
-    # growth_rate = np.max(np.array(netCDF4.Dataset(os.path.join(OUT_DIR,f"{gs2_input_name}.out.nc"),'r').variables['omega_average'][()])[-1,:,0,1])
+    max_growthrate_gamma, max_growthrate_omega, max_growthrate_ky = gammabyky(fout)
+    # gx_out = GX_Output(fout)
+    # qavg, dqavg = gx_out.exponential_window_estimator()
+    # eigenPlot(fout)
+    # growth_rate, omega = getgamma(fout)
+    # kyX, growthRateX, realFrequencyX = gammabyky(fout)
+    # growth_rate = np.max(np.array(netCDF4.Dataset(fout,'r').variables['omega_average'][()])[-1,:,0,1])
     # remove_gs2_files(gs2_input_name)
-    output_to_csv(nzgrid, npol, nstep, dt, nhermite, nlaguerre, qavg, LN, LT)
-    return qavg
+    output_to_csv(nzgrid, npol, nstep, dt, nhermite, nlaguerre, max_growthrate_gamma, max_growthrate_omega, max_growthrate_ky, LN, LT)
+    return growth_rate
 ###
 ### Run GS2
 ###
