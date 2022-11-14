0fd5276029279ea67fb11e8f5fbb6867c9e1cf04
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-11-14 13:17:07 +0000

    even better parameters

--- GX_SIMSOPT/test_parameters.py
+++ GX_SIMSOPT/test_parameters.py
@@ -35,18 +35,18 @@ if vmec_file[-5:-3]=='QA':
     npol = 1
     desired_normalized_toroidal_flux = 0.25
     alpha_fieldline = 0
-    nhermite  = 16
-    nlaguerre = 6
+    nhermite  = 18
+    nlaguerre = 8
     nu_hyper = 0.5
 else:
     nstep = 8000
     dt = 0.015
-    nzgrid = 60
+    nzgrid = 50
     npol = 2
     desired_normalized_toroidal_flux = 0.25
     alpha_fieldline = 0
-    nhermite  = 16
-    nlaguerre = 6
+    nhermite  = 18
+    nlaguerre = 10
     nu_hyper = 0.5
 ny = 30
 ########################################
