f8ee6f11f76e6aa298e75dae17176d5268ab2d3d
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-11-14 17:21:53 +0000

    ignoring nans

--- GX_SIMSOPT/test_parameters.py
+++ GX_SIMSOPT/test_parameters.py
@@ -57,8 +57,7 @@ def gammabyky(stellFile):
     omega_average_array = np.array(fX.groups['Special']['omega_v_time'][()])
     realFrequencyX = omega_average_array[-1,:,0,0] # only looking at one kx
     growthRateX = omega_average_array[-1,:,0,1] # only looking at one kx
-    growthRateX[growthRateX == np.nan] = 0
-    max_index = np.argmax(growthRateX)
+    max_index = np.nanargmax(growthRateX)
     max_growthrate_gamma = growthRateX[max_index]
     max_growthrate_omega = realFrequencyX[max_index]
     max_growthrate_ky = kyX[max_index]
