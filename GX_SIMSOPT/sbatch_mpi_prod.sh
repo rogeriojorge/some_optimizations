b2082252bcdb049004c9132f985f31fd80919a4f
Rogerio Jorge <rogerio.jorge@tecnico.ulisboa.pt>
2022-11-21 15:03:15 +0100

    starting nonlinear GX runs

--- GX_SIMSOPT/sbatch_mpi_prod.sh
+++ GX_SIMSOPT/sbatch_mpi_prod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -A FUAC6_OHARS
 #SBATCH -p m100_fua_prod
-#SBATCH --time 04:00:00     # format: HH:MM:SS
-#SBATCH -N 8                # nodes
+#SBATCH --time 12:00:00     # format: HH:MM:SS
+#SBATCH -N 12                # nodes
 #SBATCH --ntasks-per-node=4 # tasks out of 128
 #SBATCH --gres=gpu:4        # gpus per node out of 4
 #SBATCH --mem=100000        # memory per node out of 246000MB
@@ -13,4 +13,4 @@
 
 cd /m100/home/userexternal/rjorge00/some_optimizations/GX_SIMSOPT
 
-mpirun -n 32 --rank-by core ./main.py
+mpirun -n 48 --rank-by core ./main.py
