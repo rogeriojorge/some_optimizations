4f41d86e06e2d22077b228f61898393fcf05bb25
rogeriojorge <rogerio.jorge@tecnico.ulisboa.pt>
2022-12-02 16:00:35 +0100

    starting nonlines gs2 opts

---
+++ GS2_SIMSOPT_ITG/sbatch_QA_fin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH -N 12
+#SBATCH -A FUA36_OHARS
+#SBATCH -p skl_fua_prod
+#SBATCH --mem=160000 
+#SBATCH --time 12:00:00
+#SBATCH --job-name=QA_fin_nl
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=rogerio.jorge@tecnico.ulisboa.pt
+cd /marconi/home/userexternal/rjorge00/some_optimizations/GS2_SIMSOPT_ITG
+
+cd nonlinear_nfp2_QA_test_ln1.0_lt3.0
+srun -N 12 /marconi/home/userexternal/rjorge00/gs2/bin/gs2 gs2Input_ln1.0lt3.0.in > gs2Input_ln1.0lt3.0.log
---
+++ GS2_SIMSOPT_ITG/sbatch_QH_in.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH -N 12
+#SBATCH -A FUA36_OHARS
+#SBATCH -p skl_fua_prod
+#SBATCH --mem=160000 
+#SBATCH --time 12:00:00
+#SBATCH --job-name=QH_in_nl
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=rogerio.jorge@tecnico.ulisboa.pt
+cd /marconi/home/userexternal/rjorge00/some_optimizations/GS2_SIMSOPT_ITG
+
+cd nonlinear_nfp4_QH_initial_ln1.0_lt3.0
+srun -N 12 /marconi/home/userexternal/rjorge00/gs2/bin/gs2 gs2Input_ln1.0lt3.0.in > gs2Input_ln1.0lt3.0.log
