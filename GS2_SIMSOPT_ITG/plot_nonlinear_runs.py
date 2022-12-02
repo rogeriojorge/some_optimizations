45f4e81e90009aa0806506b8bf43a1e18c79f8f2
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-12-02 15:34:33 +0000

    same

--- GS2_SIMSOPT_ITG/plot_nonlinear_runs.py
+++ GS2_SIMSOPT_ITG/plot_nonlinear_runs.py
@@ -9,18 +9,22 @@ QHinitial = "/marconi/home/userexternal/rjorge00/some_optimizations/GS2_SIMSOPT_
 QHfinal = "/marconi/home/userexternal/rjorge00/some_optimizations/GS2_SIMSOPT_ITG/nonlinear_nfp4_QH_test_ln1.0_lt3.0/gs2Input_ln1.0lt3.0.out.nc"
 
 flux_QA_initial = np.array(netCDF4.Dataset(QAinitial,'r',mmap=False).variables['es_heat_flux'][()])
-flux_QA_final   = np.array(netCDF4.Dataset(QAfinal,'r',mmap=False).variables['es_heat_flux'][()])
-flux_QH_initial = np.array(netCDF4.Dataset(QHinitial,'r',mmap=False).variables['es_heat_flux'][()])
-flux_QH_final   = np.array(netCDF4.Dataset(QHfinal,'r',mmap=False).variables['es_heat_flux'][()])
+time_QA_initial = np.array(netCDF4.Dataset(QAinitial,'r',mmap=False).variables['t'][()])
 
-time = netCDF4.Dataset(QAinitial,'r',mmap=False).variables['t'][()]
+flux_QA_final   = np.array(netCDF4.Dataset(QAfinal,'r',mmap=False).variables['es_heat_flux'][()])
+time_QA_final   = np.array(netCDF4.Dataset(QAfinal,'r',mmap=False).variables['t'][()])
+
+flux_QH_initial = np.array(netCDF4.Dataset(QHinitial,'r',mmap=False).variables['es_heat_flux'][()])
+time_QH_initial = np.array(netCDF4.Dataset(QHinitial,'r',mmap=False).variables['t'][()])
+
+flux_QH_final   = np.array(netCDF4.Dataset(QHfinal,'r',mmap=False).variables['es_heat_flux'][()])
+time_QH_final   = np.array(netCDF4.Dataset(QHfinal,'r',mmap=False).variables['t'][()])
 
 plt.figure()
-plt.plot(time,flux_QA_initial,label='QA initial')
-plt.plot(time,flux_QA_final,label='QA final')
-plt.plot(time,flux_QH_initial,label='QH initial')
-plt.plot(time,flux_QH_final,label='QH final')
-
+plt.plot(time_QA_initial,flux_QA_initial,label='QA initial')
+plt.plot(time_QA_final,flux_QA_final,label='QA final')
+plt.plot(time_QH_initial,flux_QH_initial,label='QH initial')
+plt.plot(time_QH_final,flux_QH_final,label='QH final')
 
 plt.xlabel(r'$t$');plt.ylabel(r'$Q$')
 plt.legend(loc="upper right")
