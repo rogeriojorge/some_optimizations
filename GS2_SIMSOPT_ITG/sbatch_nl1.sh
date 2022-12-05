c918a75646e235f345123ce72bb0bd2db9944f05
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-12-05 19:02:30 +0000

    more cores to problem

--- GS2_SIMSOPT_ITG/sbatch_nl1.sh
+++ GS2_SIMSOPT_ITG/sbatch_nl1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH -N 48
+#SBATCH -N 128
 #SBATCH -A FUA36_OHARS
 #SBATCH -p skl_fua_prod
 #SBATCH --mem=160000 
 #SBATCH --time 12:00:00
-#SBATCH --job-name=QH_fin_nl
+#SBATCH --job-name=QA_fin_nl
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rogerio.jorge@tecnico.ulisboa.pt
 cd /marconi/home/userexternal/rjorge00/some_optimizations/GS2_SIMSOPT_ITG
-# python3 test_nonlinear.py --type 4
-cd nonlinear_nfp4_QH_final_ln1.0_lt3.0
-srun -N 48 /marconi/home/userexternal/rjorge00/gs2/bin/gs2 gs2Input_ln1.0lt3.0.in > gs2Input_ln1.0lt3.0.log
+# python3 test_nonlinear.py --type 2
+cd nonlinear_nfp2_QA_final_ln1.0_lt3.0
+srun -N 128 /marconi/home/userexternal/rjorge00/gs2/bin/gs2 gs2Input_ln1.0lt3.0.in > gs2Input_ln1.0lt3.0.log
