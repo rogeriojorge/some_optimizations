cf9a88f38795ff4c9b058fd654c02ded141a8d81
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2022-12-05 18:57:17 +0000

    plot all bu last gs2 itg nonlinear

--- GS2_SIMSOPT_ITG/plot_nonlinear_runs.py
+++ GS2_SIMSOPT_ITG/plot_nonlinear_runs.py
@@ -3,22 +3,25 @@ import netCDF4
 import numpy as np
 import matplotlib.pyplot as plt
 
-QAinitial = "/marconi/home/userexternal/rjorge00/some_optimizations/GS2_SIMSOPT_ITG/nonlinear_nfp2_QA_initial_ln1.0_lt3.0/gs2Input_ln1.0lt3.0.out.nc"
-QAfinal = "/marconi/home/userexternal/rjorge00/some_optimizations/GS2_SIMSOPT_ITG/nonlinear_nfp2_QA_final_ln1.0_lt3.0/gs2Input_ln1.0lt3.0.out.nc"
-QHinitial = "/marconi/home/userexternal/rjorge00/some_optimizations/GS2_SIMSOPT_ITG/nonlinear_nfp4_QH_initial_ln1.0_lt3.0/gs2Input_ln1.0lt3.0.out.nc"
-QHfinal = "/marconi/home/userexternal/rjorge00/some_optimizations/GS2_SIMSOPT_ITG/nonlinear_nfp4_QH_final_ln1.0_lt3.0/gs2Input_ln1.0lt3.0.out.nc"
+initial_folder = "/marconi/home/userexternal/rjorge00/"
+# initial_folder = "/Users/rogeriojorge/local/"
 
-flux_QA_initial = np.array(netCDF4.Dataset(QAinitial,'r',mmap=False).variables['es_heat_flux'][()])
-time_QA_initial = np.array(netCDF4.Dataset(QAinitial,'r',mmap=False).variables['t'][()])
+QAinitial = f"{initial_folder}some_optimizations/GS2_SIMSOPT_ITG/nonlinear_nfp2_QA_initial_ln1.0_lt3.0/gs2Input_ln1.0lt3.0.out.nc"
+QAfinal = f"{initial_folder}/some_optimizations/GS2_SIMSOPT_ITG/nonlinear_nfp2_QA_final_ln1.0_lt3.0/gs2Input_ln1.0lt3.0.out.nc"
+QHinitial = f"{initial_folder}/some_optimizations/GS2_SIMSOPT_ITG/nonlinear_nfp4_QH_initial_ln1.0_lt3.0/gs2Input_ln1.0lt3.0.out.nc"
+QHfinal = f"{initial_folder}/some_optimizations/GS2_SIMSOPT_ITG/nonlinear_nfp4_QH_final_ln1.0_lt3.0/gs2Input_ln1.0lt3.0.out.nc"
 
-flux_QA_final   = np.array(netCDF4.Dataset(QAfinal,'r',mmap=False).variables['es_heat_flux'][()])
-time_QA_final   = np.array(netCDF4.Dataset(QAfinal,'r',mmap=False).variables['t'][()])
+flux_QA_initial = np.array(netCDF4.Dataset(QAinitial,'r',mmap=False).variables['es_heat_flux'][()])[:-1]
+time_QA_initial = np.array(netCDF4.Dataset(QAinitial,'r',mmap=False).variables['t'][()])[:-1]
 
-flux_QH_initial = np.array(netCDF4.Dataset(QHinitial,'r',mmap=False).variables['es_heat_flux'][()])
-time_QH_initial = np.array(netCDF4.Dataset(QHinitial,'r',mmap=False).variables['t'][()])
+flux_QA_final   = np.array(netCDF4.Dataset(QAfinal,'r',mmap=False).variables['es_heat_flux'][()])[:-1]
+time_QA_final   = np.array(netCDF4.Dataset(QAfinal,'r',mmap=False).variables['t'][()])[:-1]
 
-flux_QH_final   = np.array(netCDF4.Dataset(QHfinal,'r',mmap=False).variables['es_heat_flux'][()])
-time_QH_final   = np.array(netCDF4.Dataset(QHfinal,'r',mmap=False).variables['t'][()])
+flux_QH_initial = np.array(netCDF4.Dataset(QHinitial,'r',mmap=False).variables['es_heat_flux'][()])[:-1]
+time_QH_initial = np.array(netCDF4.Dataset(QHinitial,'r',mmap=False).variables['t'][()])[:-1]
+
+flux_QH_final   = np.array(netCDF4.Dataset(QHfinal,'r',mmap=False).variables['es_heat_flux'][()])[:-1]
+time_QH_final   = np.array(netCDF4.Dataset(QHfinal,'r',mmap=False).variables['t'][()])[:-1]
 
 plt.figure()
 plt.plot(time_QA_initial,flux_QA_initial,label='QA initial')
@@ -29,5 +32,6 @@ plt.plot(time_QH_final,flux_QH_final,label='QH final')
 plt.xlabel(r'$t$');plt.ylabel(r'$Q$')
 plt.legend(loc="upper right")
 plt.tight_layout()
-plt.savefig('Qfluxes.pdf')
+plt.show()
+# plt.savefig('Qfluxes.pdf')
 plt.close()
