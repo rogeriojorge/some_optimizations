9e81f32962e7b26228ffb92cb6390fb6959c4bdd
rogeriojorge <rogerio.jorge@tecnico.ulisboa.pt>
2022-12-22 16:34:25 +0100

    same

--- GS2_SIMSOPT_ITG/sbatch_test_QA.sh
+++ GS2_SIMSOPT_ITG/sbatch_test_QA.sh
@@ -10,4 +10,4 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rogerio.jorge@tecnico.ulisboa.pt
 cd /marconi_scratch/userexternal/rjorge00/some_optimizations/GS2_SIMSOPT_ITG
-mpirun -n 1 ./test_parameters.py --type 2
+mpirun -n 1 ./test_parameters.py --type 1
