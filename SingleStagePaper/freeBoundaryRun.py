51f4b2099f60759e398bb443af9a9f9115915e0d
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2023-01-23 09:41:16 +0000

    commented out other things

--- SingleStagePaper/freeBoundaryRun.py
+++ SingleStagePaper/freeBoundaryRun.py
@@ -67,32 +67,32 @@ else:
 
 r0 = np.sqrt(s_final.gamma()[:, :, 0] ** 2 + s_final.gamma()[:, :, 1] ** 2)
 z0 = s_final.gamma()[:, :, 2]
-with open(os.path.join(outdir_coils,'input_xgrid.dat'), 'w') as f:
-    f.write('opt_coils\n')
-    f.write('S\n')
-    f.write('y\n')
-    f.write(f'{0.9*np.min(r0)}\n')
-    f.write(f'{1.1*np.max(r0)}\n')
-    f.write(f'{1.1*np.min(z0)}\n')
-    f.write(f'{1.1*np.max(z0)}\n')
-    f.write(f'{nzeta}\n')
-    f.write(f'{nr}\n')
-    f.write(f'{nz}\n')
+# with open(os.path.join(outdir_coils,'input_xgrid.dat'), 'w') as f:
+#     f.write('opt_coils\n')
+#     f.write('S\n')
+#     f.write('y\n')
+#     f.write(f'{0.9*np.min(r0)}\n')
+#     f.write(f'{1.1*np.max(r0)}\n')
+#     f.write(f'{1.1*np.min(z0)}\n')
+#     f.write(f'{1.1*np.max(z0)}\n')
+#     f.write(f'{nzeta}\n')
+#     f.write(f'{nr}\n')
+#     f.write(f'{nz}\n')
 
-print("Running makegrid")
-os.chdir(outdir_coils)
-run_string = f"{mgrid_executable} < {os.path.join(outdir_coils,'input_xgrid.dat')} > {os.path.join(outdir_coils,'log_xgrid.opt_coils')}"
-run(run_string, shell=True, check=True)
-os.chdir(this_path)
-print(" done")
+# print("Running makegrid")
+# os.chdir(outdir_coils)
+# run_string = f"{mgrid_executable} < {os.path.join(outdir_coils,'input_xgrid.dat')} > {os.path.join(outdir_coils,'log_xgrid.opt_coils')}"
+# run(run_string, shell=True, check=True)
+# os.chdir(this_path)
+# print(" done")
 
 vmec_final.indata.lfreeb = True
-vmec_final.indata.mgrid_file = os.path.join(outdir_coils,'mgrid_opt_coils.nc')
-# vmec_final.indata.mgrid_file = os.path.join(outdir_coils,'tomgrid_opt_coils.nc')
+# vmec_final.indata.mgrid_file = os.path.join(outdir_coils,'mgrid_opt_coils.nc')
+vmec_final.indata.mgrid_file = os.path.join(outdir_coils,'tomgrid_opt_coils.nc')
 vmec_final.indata.extcur[0:ncoils_total] = [c.current.get_value() for c in bs_final.coils]
 vmec_final.indata.nvacskip = 6
 vmec_final.indata.nzeta = nzeta
-vmec_final.indata.phiedge = -np.abs(vmec_final.indata.phiedge)
+vmec_final.indata.phiedge = -(1/(4*np.pi))*1e7*np.abs(vmec_final.indata.phiedge)
 
 vmec_final.indata.ns_array[:4]    = [   9,    29,    49,   101]
 vmec_final.indata.niter_array[:4] = [4000,  6000,  6000,  8000]
@@ -100,13 +100,15 @@ vmec_final.indata.ftol_array[:4]  = [1e-5,  1e-6, 1e-12, 1e-15]
 
 vmec_final.write_input(os.path.join(dir,'input.final_freeb'))
 
-print("Running VMEC")
-os.chdir(os.path.join(dir,'vmec'))
-run_string = f"{vmec_executable} {os.path.join(dir,'input.final_freeb')}"
-run(run_string, shell=True, check=True)
-try: shutil.move(os.path.join(dir, 'vmec', f"wout_final_freeb.nc"), os.path.join(dir, f"wout_final_freeb.nc"))
-except Exception as e: print(e)
-os.chdir(this_path)
+vmec_final.run()
+
+# print("Running VMEC")
+# os.chdir(os.path.join(dir,'vmec'))
+# run_string = f"{vmec_executable} {os.path.join(dir,'input.final_freeb')}"
+# run(run_string, shell=True, check=True)
+# try: shutil.move(os.path.join(dir, 'vmec', f"wout_final_freeb.nc"), os.path.join(dir, f"wout_final_freeb.nc"))
+# except Exception as e: print(e)
+# os.chdir(this_path)
 
 boozxform_nsurfaces=10
 helical_detail=True
